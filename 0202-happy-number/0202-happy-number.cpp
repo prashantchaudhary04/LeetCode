4a6ddb5f397c6341101a5e7ab455c16da439e4ac
Prashant Chaudhary <prashant020805@gmail.com>
2026-09-12 22:01:45 +0530

    Time: 0 ms (100%), Space: 7.7 MB (96.88%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
+
+    int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit; 
+            n /= 10;
+        }
+        return sum;
+    }
+};
